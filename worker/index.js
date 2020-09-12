34c5ea68cc3d07d9a06c6320242662c012771d8f
Anirban Gangopadhyay <anirban.gangopadhyay@oracle.com>
2020-09-12 22:56:51 +0530

    testing

--- worker/index.js
+++ worker/index.js
@@ -14,6 +14,8 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2);
 }
 
+// just testing:
+
 sub.on('message', (channel, message) => {
   redisClient.hset('values', message, fib(parseInt(message)));
 });
